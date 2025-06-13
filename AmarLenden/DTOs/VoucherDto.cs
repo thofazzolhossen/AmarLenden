ec720f203c07b035d406bd85c5db70a9f031bfed
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-13 13:07:51 +0600

    added voucher dto and voucher line dto

---
+++ AmarLenden/DTOs/VoucherDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AmarLenden.DTOs
+{
+    public class VoucherDto
+    {
+        public int Id { get; set; }
+        public string ReferenceNo { get; set; }
+        public DateTime Date { get; set; }
+        public string VoucherType { get; set; }
+        public List<VoucherLineDto> Lines { get; set; }
+    }
+}
