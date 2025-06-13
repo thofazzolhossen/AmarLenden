47eeb18184371771d96efa16651c6cfde9b41300
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-13 12:54:11 +0600

    added voucher view model and voucher list view model

---
+++ AmarLenden/ViewModels/VoucherVM.cs
@@ -0,0 +1,13 @@
+﻿using AmarLenden.Model;
+
+namespace AmarLenden.ViewModels
+{
+    public class VoucherVM
+    {
+        public string? ReferenceNo { get; set; }
+        public DateTime Date { get; set; }
+        public string VoucherType { get; set; }
+
+        public List<VoucherLineVM> Lines { get; set; } = new();
+    }
+}
