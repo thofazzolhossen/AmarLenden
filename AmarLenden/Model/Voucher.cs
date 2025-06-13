aa3f9615038ef17bdbd0bafbc31a4b4cb5365c18
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-13 12:47:07 +0600

    Update Voucher Model

--- AmarLenden/Model/Voucher.cs
+++ AmarLenden/Model/Voucher.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string? ReferenceNo { get; set; }
         public DateTime Date { get; set; }
-        public string VoucherType { get; set; }
+        public string? VoucherType { get; set; }
 
         public List<VoucherLine> Lines { get; set; } = new();
     }
