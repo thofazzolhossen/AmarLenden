04354a6f6b1cd6a6eb8f1d0dd2fbd93ed8d330da
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-17 14:47:13 +0600

    Update Migration

--- AmarLenden/Model/ApplicationUser.cs
+++ AmarLenden/Model/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmarLenden.Model
 {
     public class ApplicationUser: IdentityUser
     {
+        [Required]
         public string? FullName { get; set; }
     }
 }
