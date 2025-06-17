f75a16759c238ac54c7b6b8becb05166835b5175
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-17 23:32:26 +0600

    add login view model

---
+++ AmarLenden/ViewModels/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace AmarLenden.ViewModels
+{
+    public class LoginVM
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
