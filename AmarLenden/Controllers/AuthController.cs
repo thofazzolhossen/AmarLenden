e196611be4752afdbf9ea1fb1df62c9bcd118210
Thofazzol Hossen <thofazzolhossen@gmail.com>
2025-06-21 14:38:31 +0600

    Modify  Get User

--- AmarLenden/Controllers/AuthController.cs
+++ AmarLenden/Controllers/AuthController.cs
@@ -41,7 +41,53 @@ namespace AmarLenden.Controllers
             return BadRequest(ModelState);
         }
 
+        [HttpGet("users")]
+        public IActionResult GetAllUsers(int pageNumber = 1, int pageSize = 10)
+        {
+            var query = _userManager.Users.Select(u => new
+            {
+                u.Id,
+                u.FullName,
+                u.UserName,
+                u.Email
+            });
 
+            var totalUsers = query.Count();
+
+            var users = query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var result = new
+            {
+                TotalUsers = totalUsers,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                Users = users
+            };
+
+            return Ok(result);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+                return Unauthorized("Invalid username or password.");
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+            if (!result.Succeeded)
+                return Unauthorized("Invalid username or password.");
+
+            return Ok("Login successful");
+        }
 
     }
 }
